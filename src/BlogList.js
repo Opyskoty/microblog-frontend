90e409743fe4e9abeaa992f5d701cbb74e5a8341
Olivia Pyskoty <olivia.pyskoty@gmail.com>
2020-08-09 16:22:41 -0400

    fix useEffect so new posts load

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,31 +1,44 @@
-import React, { useEffect } from 'react'
-import BlogCard from "./BlogCard"
+import React, { useEffect, useState } from "react";
+import BlogCard from "./BlogCard";
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
 import { loadPostsFromApi } from "./actions";
 import { Row, Col } from "reactstrap";
 
 function BlogList() {
   const { titles } = useSelector((store) => store, shallowEqual);
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadPostsFromApi())
-  }, [dispatch]);
+    async function loadPost() {
+      dispatch(loadPostsFromApi());
+      setIsLoading(false);
+    }
+
+    if (isLoading) {
+      loadPost();
+    }
+  }, [dispatch, isLoading]);
 
   return (
     <div className="BlogList">
       <Row>
         <Col>
-          <p className="mb-4">Welcome to <b>Microblog</b>, Olivia, Marco and Harry's safe space for expressing your feelings :)</p>
+          <p className="mb-4">
+            Welcome to <b>Microblog</b>, Olivia, Marco and Harry's safe space
+            for expressing your feelings :)
+          </p>
         </Col>
       </Row>
       <Row>
-        {titles.sort((a, b) => b.votes - a.votes).map(post => (
-          <BlogCard post={post} key={post.id} />
-        ))}
+        {titles
+          .sort((a, b) => b.votes - a.votes)
+          .map((post) => (
+            <BlogCard post={post} key={post.id} />
+          ))}
       </Row>
     </div>
-  )
+  );
 }
 
 export default BlogList;
